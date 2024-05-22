633c672064f8463355d8668e043eb070a0f36a78
jieunsse <jieunsse@gmail.com>
2024-05-22 19:37:39 +0900

    refactor: 메인페이지 화면 추가 => 메인페이지 안내 문구 추가함

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,7 +1,14 @@
 import NavbarUI from '../component/NavbarUI.tsx';
 
 const MainPage = () => {
-  return <NavbarUI />;
+  return (
+    <>
+      <NavbarUI />;
+      <div className="justify-center flex mt-80 font-bold text-2xl">
+        이 페이지는 메인 페이지
+      </div>
+    </>
+  );
 };
 
 export default MainPage;
