0d9b3849f1cc8061a6265fc685ac0fecdd397acd
jieunsse <jieunsse@gmail.com>
2024-05-22 19:40:03 +0900

    chore: 오타수정 => MainPage.tsx

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -3,7 +3,7 @@ import NavbarUI from '../component/NavbarUI.tsx';
 const MainPage = () => {
   return (
     <>
-      <NavbarUI />;
+      <NavbarUI />
       <div className="justify-center flex mt-80 font-bold text-2xl">
         이 페이지는 메인 페이지
       </div>
